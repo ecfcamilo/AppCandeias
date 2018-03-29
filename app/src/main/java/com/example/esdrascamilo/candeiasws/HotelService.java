567d195b8fd9d69b7087daa3b8b6a2b82ebe6ca6
unknown <ecf.camilo@gmail.com>
2018-03-29 18:32:07 -0300

    Projeto Teste

---
+++ app/src/main/java/com/example/esdrascamilo/candeiasws/HotelService.java
@@ -0,0 +1,18 @@
+package com.example.esdrascamilo.candeiasws;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.GET;
+import retrofit2.http.PATCH;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+/**
+ * Created by Esdras Camilo on 28/03/2018.
+ */
+
+public interface HotelService {
+
+    @GET("/v1/hotels/autocomplete")
+    Call<HotelSug> getHoteis(@Query("search") String name);
+}
